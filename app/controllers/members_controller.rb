d363cd814a437788cfbaf6564891b16b58730cfd
zackieee <zackie.jackie.222@gmail.com>
2019-07-07 12:44:29 +0900

    add user/group/message method

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,9 @@
+class MembersController < ApplicationController
+
+  def index
+  end
+
+  def create
+  end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :groups, through: :members
+  has_many :messages
+  has_many :members
+end
