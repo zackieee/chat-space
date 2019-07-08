fd832617c6c5500b2a5aee90b1a51ace5697f8a2
zackieee <zackie.jackie.222@gmail.com>
2019-07-08 16:25:49 +0900

    craete ajax file

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,56 @@
+$(document).on('turbolinks:load', function() { 
+  $(function(){
+    function buildHTML(message){
+      var html = `<div class="message">
+                    <div class="message__upper-info">
+                      <p class="message__upper-info__talker">
+                        ${message.name}
+                      </p>
+                      <p class="message__upper-info__date"> 
+                        ${message.created_at}
+                      </p>
+                    </div>
+                    <p class="message__text">${message.body}</p>`
+
+      if(message.image.url !== null){
+        html +=`  <img class="message__image" src="${message.image.url}">
+                </div>`
+      }else {
+        html +=`</div>`
+      }
+      return html;
+    }
+    function scrollBottom(){
+      var target = $('.message').last();
+      var position = target.offset().top + $('.messages').scrollTop();
+      $('.messages').animate({
+        scrollTop: position
+      }, 2000, 'swing');
+    }
+    $('#new_message').on('submit',function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      $.ajax({
+        url:          $(this).attr('action'),
+        type:         "POST",
+        data:         formData,
+        dataType:     'json',
+        processData:  false,
+        contentType:  false
+      }, {passive: true})
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        $('.input-box__text').val('');
+        $('.input-box__file').val('');
+        scrollBottom();
+      })
+      .fail(function(){
+        alert('メッセージを入力してください');
+      })
+      .always(function(){
+        $('.submit-btn').removeAttr("disabled");
+      });
+    });
+  });
+});
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,12 +8,8 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.create(message_params)
-    if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
-    else
-      @messages = @group.messages.includes(:user)
-      flash.now[:alert] = 'メッセージを入力してください'
-      render :index
+    respond_to do |format|
+      format.json
     end
   end
 
