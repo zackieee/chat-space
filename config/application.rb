bf9f52e53914ddee48eb75154af766b4aadae013
zackieee <zackie.jackie.222@gmail.com>
2019-07-19 10:22:22 +0900

    change

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -17,9 +20,5 @@ module ChatSpace
       g.helper false
       g.test_framework false
     end
-
-    config.time_zone = 'Tokyo'
-    config.i18n.default_locale = :ja
-    config.active_record.default_timezone = :local
   end
 end
